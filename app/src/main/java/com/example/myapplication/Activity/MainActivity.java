071f3a673b6228a89dfa9c31d1c6c016933d681c
chien <chiennvph06113@fpt.edu.vn>
2019-07-28 17:23:57 +0700

    Fix Err

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.myapplication.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
+
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
@@ -17,43 +16,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.example.myapplication.Model.ModelCatagory;
 import com.example.myapplication.R;
 import com.example.myapplication.fragment.Fragment_AboutUs;
 import com.example.myapplication.fragment.Fragment_Category;
 import com.example.myapplication.fragment.Fragment_Latest;
 import com.example.myapplication.fragment.Fragment_MyFavorite;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.List;
 
-    private Fragment_AboutUs fragment_aboutUs;
-    private Fragment_Category fragment_category;
-    private Fragment_Latest fragment_latest;
-    private Fragment_MyFavorite fragment_myFavorite;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction ft;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        myFindViewById();
+
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new Fragment_Latest());
+        ft.commit();
+
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_latest);
+    }
 
-        fragment_latest = new Fragment_Latest();
-        fragment_myFavorite = new Fragment_MyFavorite();
-        fragment_aboutUs = new Fragment_AboutUs();
-        fragment_category = new Fragment_Category();
+    private void myFindViewById() {
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        updateTitleToolbarOnBackPress();
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -63,7 +69,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -74,7 +79,6 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -83,135 +87,66 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-
+        Fragment fragment = null;
+        Class fragmentClass;
 
         switch (item.getItemId()) {
             case R.id.nav_latest:
-                showLatest();
+                fragmentClass = Fragment_Latest.class;
                 break;
             case R.id.nav_category:
-                showCategory();
+                fragmentClass = Fragment_Category.class;
                 break;
             case R.id.nav_heart:
-                showMyFavorite();
+                fragmentClass = Fragment_MyFavorite.class;
                 break;
             case R.id.nav_about_us:
-                showAboutUs();
+                fragmentClass = Fragment_AboutUs.class;
                 break;
             default:
-                showAboutUs();
+                fragmentClass = Fragment_Latest.class;
+                break;
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        fragmentManager = getSupportFragmentManager();
+        ft = fragmentManager.beginTransaction();
+        ft.addToBackStack(fragmentClass.getName());
+        ft.replace(R.id.container, fragment).commit();
+
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        drawer.closeDrawers();
+
         return true;
     }
 
-    public void showLatest() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
+    private void updateTitleToolbarOnBackPress(){
 
-
-        if (fragment_latest.isAdded()) {
-            ft.show(fragment_latest);
-        } else {
-            ft.add(R.id.container, fragment_latest);
-        }
-
-
-        if (fragment_aboutUs.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_aboutUs);
-        }
-        if (fragment_category.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_category);
-        }
-        if (fragment_myFavorite.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_myFavorite);
-        }
-
-        //Sau khi thay đổi fragment phải xác thực lại
-        ft.commit();
-    }
-
-    public void showCategory() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
-
-
-        if (fragment_category.isAdded()) {
-            ft.show(fragment_category);
-        } else {
-            ft.add(R.id.container, fragment_category);
-        }
-
-
-        if (fragment_aboutUs.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_aboutUs);
-        }
-        if (fragment_latest.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_latest);
-        }
-        if (fragment_myFavorite.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_myFavorite);
-        }
-
-        //Sau khi thay đổi fragment phải xác thực lại
-        ft.commit();
-    }
-
-    public void showAboutUs() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
-
-
-        if (fragment_aboutUs.isAdded()) {
-            ft.show(fragment_aboutUs);
-        } else {
-            ft.add(R.id.container, fragment_aboutUs);
-        }
-
-
-        if (fragment_latest.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_latest);
-        }
-        if (fragment_category.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_category);
-        }
-        if (fragment_myFavorite.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_myFavorite);
-        }
-
-        //Sau khi thay đổi fragment phải xác thực lại
-        ft.commit();
-    }
-
-    public void showMyFavorite() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
-
-
-        if (fragment_myFavorite.isAdded()) {
-            ft.show(fragment_myFavorite);
-        } else {
-            ft.add(R.id.container, fragment_myFavorite);
-        }
-
-
-        if (fragment_aboutUs.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_aboutUs);
-        }
-        if (fragment_category.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_category);
-        }
-        if (fragment_latest.isAdded()) { //Nếu đang hiển thị thì ẩn đi
-            ft.hide(fragment_latest);
-        }
-
-        //Sau khi thay đổi fragment phải xác thực lại
-        ft.commit();
+        fragmentManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                Fragment fr = getSupportFragmentManager().findFragmentById(R.id.container);
+                if(fr!=null){
+                    if (fr.getClass().getSimpleName().equalsIgnoreCase("Fragment_Latest")){
+                        setTitle(navigationView.getMenu().getItem(0).getTitle());
+                    } else if (fr.getClass().getSimpleName().equalsIgnoreCase("Fragment_Category")){
+                        setTitle(navigationView.getMenu().getItem(1).getTitle());
+                    }else if (fr.getClass().getSimpleName().equalsIgnoreCase("Fragment_MyFavorite")){
+                        setTitle(navigationView.getMenu().getItem(2).getTitle());
+                    }else if (fr.getClass().getSimpleName().equalsIgnoreCase("Fragment_AboutUs")){
+                        setTitle(navigationView.getMenu().getItem(3).getTitle());
+                    }
+                }
+            }
+        });
     }
 }
